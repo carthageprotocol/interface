b8a94e962e850999609da48d2e7ed98ef61fa1d4
samisbakedham <samsafahi@gmail.com>
2022-07-12 18:02:12 -0700

    Update tokens.ts

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -46,14 +46,14 @@ export const USDC_CANDLE = new Token(
   'USDC',
   'USD//C'
 )
-export const USDC_SHIBA = new Token(
+export const SHIBA_CANDLE = new Token(
   SupportedChainId.CANDLE,
   '0xa018034190943D6c8E10218d9F8E8Af491272411',
   18,
   'SHIBA',
   'SHIBA'
 )
-export const USDC_ZED = new Token(
+export const ZED_CANDLE = new Token(
   SupportedChainId.CANDLE,
   '0x5c17C48F127D6aE5794b2404F1F8A5CeED419eDf',
   6,
